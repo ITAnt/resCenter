a3c9fe82692834efac4fa1c7d36b210e4a51ba48
lzlbuilder <18942260268@163.com>
2021-07-01 12:21:01 +0800

    完成<实时语音处理实践指南>

--- matlab/caldiffenergy.m
+++
@@ -1,8 +0,0 @@
-function diffenergy = caldiffenergy(energy)
-v = diff(energy);
-[x,y] = size(v);
-for i=1:y
-    zero(i) = 0;
-end
-diffenergy = abs(([zero;v]));
-end
--- matlab/基础知识/test1.m
+++
@@ -1,5 +0,0 @@
-clear all;
-eq = 'D2y+3*Dy+2*y=0';
-cond = 'y(0)= 1,Dy(0)=2';
-yzi = dsolve(eq,cond);
-yzi = simplify(yzi)
